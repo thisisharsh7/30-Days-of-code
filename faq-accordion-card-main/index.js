0279818a34da53764cbc33e4b9f38fe320b3fb93
Harsh Kumar <9u.harsh@gmail.com>
2022-08-26 16:31:12 +0530

    project 19 on go

---
+++ faq-accordion-card-main/index.js
@@ -0,0 +1,8 @@
+console.log("harsh is here");
+let btn=document.querySelectorAll(".arrow");
+console.log(btn);
+btn.forEach(sbtn =>{
+    sbtn.addEventListener('click',()=>{
+        console.log("jadu");
+    })
+})
