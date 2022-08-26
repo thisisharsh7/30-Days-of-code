8070608e50d95cfc133ca286e80e12c30268fc24
Harsh Kumar <9u.harsh@gmail.com>
2022-08-26 19:32:04 +0530

    project 19 on go

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -3,9 +3,22 @@ const btn=document.querySelectorAll(".arrow");
 const div=document.querySelectorAll(".first");
 console.log(btn);
 btn.forEach(sbtn =>{
-    sbtn.addEventListener('click',()=>{
+    sbtn.addEventListener('click',() =>{
+        div.forEach(myFunction);
         const x = sbtn.id;
-        const el=document.getElementById(`${div[x-1].lastElementChild.id}`)
-        el.style.display="block";
+        // const x = sbtn.id;
+        // const el=document.getElementById(`${div[x-1].lastElementChild.id}`)
+        // el.style.display="block";
+        // element.style.display="block";
+        function myFunction(value){
+            const x = value.children[1].id;
+            const i = sbtn.parentElement.nextElementSibling.id;
+            const el=document.getElementById(`${x}`);
+            if(x==i){
+                el.style.display="block";
+            }else{
+                el.style.display="none";
+            }
+        }
     })
 })
