3b9e2285a670ebe0a34b5f176e68ad6adae34a1a
Harsh Kumar <9u.harsh@gmail.com>
2022-08-27 16:29:37 +0530

    project 20 on go

--- intro-component-with-signup-form-master/index.js
+++ intro-component-with-signup-form-master/index.js
@@ -1,7 +1,8 @@
 const sbmit = document.getElementById("btn");
 const input = document.querySelectorAll(".inp");
 const txt = " cannot be empty";
-var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var validRegex =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 var letters = /^[A-Za-z]+$/;
 sbmit.addEventListener("click", (e) => {
   let k = 0;
@@ -10,32 +11,29 @@ sbmit.addEventListener("click", (e) => {
     if (element.value.length == 0) {
       element.placeholder = "";
       element.parentElement.className = "invalid";
-      if (element.id == "email") {
-        element.placeholder = "email@example/com";
-        p.innerHTML = "Looks like this is not an email";
-      } else {
-        p.innerHTML = `${element.id + txt}`;
-      }
+      p.innerHTML = `${element.name + txt}`;
       p.className = "error";
     } else {
-      if (element.id == "Password" && element.value.length < 6) {
+      if (element.name == "Password" && element.value.length < 6) {
         element.parentElement.className = "invalid";
         p.innerHTML = "Password must be atleast 6 characters long";
         element.value = "";
         p.className = "error";
-      }else if(element.id == "email" && !email.value.match(validRegex)){
+      } else if (element.name == "Email" && !email.value.match(validRegex)) {
         element.parentElement.className = "invalid";
         element.placeholder = "email@example/com";
-        element.value="";
+        element.value = "";
         p.innerHTML = "Looks like this is not an email";
         p.className = "error";
-      }else if((element.id== "First Name" || element.id=="Last Name") && !element.value.match(letters)){
+      } else if (
+        (element.name == "First Name" || element.name == "Last Name") &&
+        !element.value.match(letters)
+      ) {
         element.parentElement.className = "invalid";
         element.placeholder = "";
         p.innerHTML = "Looks like this is not a name";
         p.className = "error";
-      }
-       else {
+      } else {
         p.className = "";
         element.parentElement.className = "";
         element.style.borderColor = "1px solid hsl(249, 10%, 26%)";
@@ -44,7 +42,7 @@ sbmit.addEventListener("click", (e) => {
     }
   });
   if (k == 4) {
-      window.location.reload();
+    window.location.reload();
   }
   e.preventDefault();
 });
