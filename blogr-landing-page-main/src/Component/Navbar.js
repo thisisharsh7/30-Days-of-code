e4e46cff71b5ac4481594ef93f58b9f31063d87d
Harsh Kumar <9u.harsh@gmail.com>
2022-09-07 00:45:55 +0530

    project 30 on go

--- blogr-landing-page-main/src/Component/Navbar.js
+++ blogr-landing-page-main/src/Component/Navbar.js
@@ -5,28 +5,55 @@ import CloseIcon from "./images/icon-close.svg";
 import HamburgerIcon from "./images/icon-hamburger.svg";
 
 function Navbar() {
-  const [showClick, scanClick] = useState("none");
-  function expandTab() {
-    if (showClick === "none") {
-      scanClick("flex");
-    } else {
-      scanClick("none");
+  const [showProduct, getProduct] = useState("none");
+  const [showCompany , getCompany] = useState("none");
+  const [showConnect , getConnect] = useState("none");
+  const [closeButton , showButton] = useState ("none");
+  const [openButton , showOpenButton] = useState("block");
+
+
+  function expandTab(event) {
+    const k= event.target.parentElement.id;
+    if(k==1 && showProduct=="none"){
+      getProduct("flex");
+      getCompany("none");
+      getConnect("none");
+    }else if(k==2 && showCompany=="none"){
+      getCompany("flex");
+      getProduct("none");
+      getConnect("none");
+    }else if(k==3 && showConnect=="none"){
+      getProduct("none");
+      getCompany("none");
+      getConnect("flex");
+    }else{
+      getProduct("none");
+      getCompany("none");
+      getConnect("none");
     }
+  }
+  function hamClick(){
+    showButton("block");
+    showOpenButton("none");
+  }
+  function closeClick(){
+    showButton("none");
+    showOpenButton("block");
   }
   return (
     <>
       <nav>
         <img src={logo} alt="Blogr_logo" />
-        <img id="close" src={CloseIcon} alt="close" />
-          <img id="hamburger" src={HamburgerIcon} alt="hamburger" />
+        <img id="close" src={CloseIcon} style={{display: closeButton}} alt="close" onClick={closeClick}/>
+          <img id="hamburger" onClick={hamClick} style={{display: openButton}}  src={HamburgerIcon} alt="hamburger" />
         <div id="nav-static">
           <div id="subNav">
             <div className="merge">
-              <button onClick={expandTab}>
+              <button type="button" onClick={expandTab} id={1}>
                 <span>Product</span>
                 <img src={Arrow} alt="arrow" />
               </button>
-              <ul id="product" style={{ display: showClick }}>
+              <ul id="product" style={{ display: showProduct }}>
                 <li> Overview</li>
                 <li> Pricing</li>
                 <li> Marketplace</li>
@@ -35,11 +62,11 @@ function Navbar() {
               </ul>
             </div>
             <div className="merge">
-              <button onClick={expandTab}>
+              <button type="button" onClick={expandTab} id={2}>
                 <span>Company</span>
                 <img src={Arrow} alt="arrow" />
               </button>
-              <ul id="company" style={{ display: showClick }}>
+              <ul id="company" style={{ display: showCompany }}>
                 <li> About</li>
                 <li> Team</li>
                 <li> Blog</li>
@@ -47,11 +74,11 @@ function Navbar() {
               </ul>
             </div>
             <div className="merge">
-              <button onClick={expandTab}>
+              <button type="button" onClick={expandTab} id={3}>
                 <span>Connect</span>
                 <img src={Arrow} alt="arrow" />
               </button>
-              <ul className="connect" style={{ display: showClick }}>
+              <ul className="connect" style={{ display: showConnect }}>
                 <li> Contact</li>
                 <li> Newsletter</li>
                 <li> LinkedIn</li>
