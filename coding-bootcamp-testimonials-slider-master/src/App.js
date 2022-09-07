5860bc3fd87efc58ac42acf08e01c641384bac49
Harsh Kumar <9u.harsh@gmail.com>
2022-09-07 19:12:35 +0530

    project 31 on go

---
+++ coding-bootcamp-testimonials-slider-master/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ coding-bootcamp-testimonials-slider-master/src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div id="attribution">
+        <p>
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by
+          <a href="https://www.frontendmentor.io/profile/thisisharsh7">
+            Harsh Kumar
+          </a>
+          .
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
