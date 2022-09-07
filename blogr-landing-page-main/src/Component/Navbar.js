60a17877ad2a8064338956af3a8079f4cff3d5ec
Harsh Kumar <9u.harsh@gmail.com>
2022-09-07 08:59:37 +0530

    project 30 on go

--- blogr-landing-page-main/src/Component/Navbar.js
+++ blogr-landing-page-main/src/Component/Navbar.js
@@ -1,51 +1,111 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import logo from "./images/logo.svg";
 import Arrow from "./images/icon-arrow-light.svg";
 import CloseIcon from "./images/icon-close.svg";
 import HamburgerIcon from "./images/icon-hamburger.svg";
 
 function Navbar() {
-  const [showProduct, getProduct] = useState("none");
-  const [showCompany , getCompany] = useState("none");
-  const [showConnect , getConnect] = useState("none");
-  const [closeButton , showButton] = useState ("none");
-  const [openButton , showOpenButton] = useState("block");
-
+  const [size , setSize] = useState(window.innerWidth);
+  const [show, get] = useState({
+    product: "none",
+    company: "none",
+    connect: "none",
+  });
+  const [storeButton, setToggle] = useState({
+    hamb: "block",
+    cross: "none",
+  })
+  if(window.innerWidth>748){
+    storeButton.hamb="none";
+    storeButton.cross="none";
+  }
+  useLayoutEffect(()=>{
+    window.addEventListener('resize',()=>{
+      setSize(window.innerWidth);
+    })
+    if(size>748){
+      document.getElementById("nav-static").style.display="flex";
+      storeButton.hamb="none";
+      storeButton.cross="none";
+    }else{
+      if(storeButton.cross==="none"){
+        document.getElementById("nav-static").style.display="none";
+        storeButton.hamb="block";
+      }
+    }
+    return () => window.removeEventListener('resize',()=>{
+      setSize(window.innerWidth);
+    })
+  })
 
   function expandTab(event) {
-    const k= event.target.parentElement.id;
-    if(k==1 && showProduct=="none"){
-      getProduct("flex");
-      getCompany("none");
-      getConnect("none");
-    }else if(k==2 && showCompany=="none"){
-      getCompany("flex");
-      getProduct("none");
-      getConnect("none");
-    }else if(k==3 && showConnect=="none"){
-      getProduct("none");
-      getCompany("none");
-      getConnect("flex");
-    }else{
-      getProduct("none");
-      getCompany("none");
-      getConnect("none");
-    }
+    const k = event.target.parentElement.id;
+    
+    get(() => {
+      if (k === "1" && show.product === "none") {
+        return {
+          product: "flex",
+          company: "none",
+          connect: "none",
+        };
+      } else if (k === "2" && show.company === "none") {
+        return {
+          product: "none",
+          company: "flex",
+          connect: "none",
+        };
+      } else if (k === "3" && show.connect === "none") {
+        return {
+          product: "none",
+          company: "none",
+          connect: "flex",
+        };
+      } else {
+        return {
+          product: "none",
+          company: "none",
+          connect: "none",
+        };
+      }
+    });
   }
-  function hamClick(){
-    showButton("block");
-    showOpenButton("none");
-  }
-  function closeClick(){
-    showButton("none");
-    showOpenButton("block");
+  function toggleButton(event) {
+    setToggle(() => {
+      if (event.target.id === "close") {
+        document.getElementById("nav-static").className="";
+        document.getElementById("nav-static").style.display="none";
+        return {
+          hamb: "block",
+          cross: "none",
+        };
+      } else {
+        document.getElementById("nav-static").className="nav-change";
+        document.getElementById("nav-static").style.display="flex";
+        return {
+          hamb: "none",
+          cross: "block",
+        };
+      }
+    });
   }
   return (
     <>
       <nav>
         <img src={logo} alt="Blogr_logo" />
-        <img id="close" src={CloseIcon} style={{display: closeButton}} alt="close" onClick={closeClick}/>
-          <img id="hamburger" onClick={hamClick} style={{display: openButton}}  src={HamburgerIcon} alt="hamburger" />
+        <img
+          id="close"
+          src={CloseIcon}
+          style={{ display: storeButton.cross }}
+          alt="close"
+          onClick={toggleButton}
+        />
+        <img
+          id="hamburger"
+          onClick={toggleButton}
+          style={{ display: storeButton.hamb }}
+          src={HamburgerIcon}
+          alt="hamburger"
+        />
         <div id="nav-static">
           <div id="subNav">
             <div className="merge">
@@ -53,7 +113,7 @@ function Navbar() {
                 <span>Product</span>
                 <img src={Arrow} alt="arrow" />
               </button>
-              <ul id="product" style={{ display: showProduct }}>
+              <ul id="product" style={{ display: show.product }}>
                 <li> Overview</li>
                 <li> Pricing</li>
                 <li> Marketplace</li>
@@ -66,7 +126,7 @@ function Navbar() {
                 <span>Company</span>
                 <img src={Arrow} alt="arrow" />
               </button>
-              <ul id="company" style={{ display: showCompany }}>
+              <ul id="company" style={{ display: show.company }}>
                 <li> About</li>
                 <li> Team</li>
                 <li> Blog</li>
@@ -78,7 +138,7 @@ function Navbar() {
                 <span>Connect</span>
                 <img src={Arrow} alt="arrow" />
               </button>
-              <ul className="connect" style={{ display: showConnect }}>
+              <ul className="connect" style={{ display: show.connect }}>
                 <li> Contact</li>
                 <li> Newsletter</li>
                 <li> LinkedIn</li>
