0c73a1654c657bbff7464202a7d0352e9acb7e15
Harsh Kumar <9u.harsh@gmail.com>
2022-09-08 12:19:18 +0530

    project 32 on go

---
+++ pricing-component-with-toggle-master/copy.txt
@@ -0,0 +1,28 @@
+
+  Our Pricing
+  Annually
+  Monthly
+
+  Basic
+  &dollar;19.99
+  &dollar;199.99
+  500 GB Storage
+  2 Users Allowed
+  Send up to 3 GB
+  Learn More
+
+  Professional
+  &dollar;24.99
+  &dollar;249.99
+  1 TB Storage
+  5 Users Allowed
+  Send up to 10 GB
+  Learn More
+
+  Master
+  &dollar;39.99
+  &dollar;399.99
+  2 TB Storage
+  10 Users Allowed
+  Send up to 20 GB
+  Learn More
---
+++ pricing-component-with-toggle-master/src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by
+          <a href="https://www.frontendmentor.io/profile/thisisharsh7">
+            Harsh Kumar
+          </a>
+          .
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
