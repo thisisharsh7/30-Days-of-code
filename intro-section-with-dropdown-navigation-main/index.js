681f8885498bf5ddcdccab42cdf80a4d92d6fb5e
Harsh Kumar <9u.harsh@gmail.com>
2022-09-10 10:29:32 +0530

    project 34 on go

--- intro-section-with-dropdown-navigation-main/index.js
+++ intro-section-with-dropdown-navigation-main/index.js
@@ -14,13 +14,20 @@ function onExapnd(event){
         event.nextElementSibling.classList.toggle("sub-bie");
         upAir.toggle("showArrow");
         downAir.toggle("showArrow");
+       
     })
 }
 function myFunction(el){
-    el.addEventListener("click",()=>{
+    el.addEventListener("click",(event)=>{
         side[0].classList.toggle("hamToggle");
         side[1].classList.toggle("crossToggle");
-        console.log(nav);
         nav.classList.toggle("show-nav-items");
+        document.body.classList.toggle("overlay");
+        if(event.target.getAttribute("alt")=="close"){
+            document.querySelector("main").classList.add("mainAnimate");
+        }else{
+            document.querySelector("main").classList.remove("mainAnimate");
+        }
+        
     })
 }
