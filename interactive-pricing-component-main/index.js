7f95197c1c7015a187085f9f6f46fa12a6136164
Harsh Kumar <9u.harsh@gmail.com>
2022-09-11 08:33:01 +0530

    project 35 on go

--- interactive-pricing-component-main/index.js
+++ interactive-pricing-component-main/index.js
@@ -1,23 +1,29 @@
 const btn = document.querySelector(".toggle-inner");
 const rate = document.getElementById("charge");
-const sliding = document.querySelector(".slider-surface");
 const slide = document.querySelector(".slider-inner");
+const sliding = document.getElementById("slider-outer");
+const slidSurface = document.querySelector(".slider-surface");
 
-const mval = 16.00;
-const yval = mval*12 - (mval*12*25)/100;
+let mval = 16.00;
+let yval = mval*12 - (mval*12*25)/100;
 
+
+sliding.addEventListener("click",e=>{
+    slidSurface.style.width=`${e.offsetX}px`;
+    slide.style.left=`${e.offsetX>40? e.offsetX-25: e.offsetX-20}px`;
+})
+
+window.addEventListener("resize",()=>{
+    slidSurface.style.width="40px";
+    slide.style.left="40px";
+})
 
 btn.addEventListener("click",(event)=>{
     const el = event.target.classList;
     el.toggle("toggle-right");
     if(el.length==1){
-        rate.innerText="$" + Number(mval).toFixed(2);
+        rate.innerText=`$${Number(mval).toFixed(2)}`;
     }else{
-        rate.innerText="$" + Number(yval).toFixed(2);
+        rate.innerText=`$${Number(yval).toFixed(2)}`;
     }
 })
-sliding.addEventListener("click",e=>{
-    console.log(e);
-    e.target.parentElement.style.left=80;
-})
-
