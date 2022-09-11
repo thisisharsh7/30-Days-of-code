be06f3ce6ef2d927140ad4398f8fa2b19ec961ad
Harsh Kumar <9u.harsh@gmail.com>
2022-09-11 10:30:21 +0530

    project 35 on go

--- interactive-pricing-component-main/index.js
+++ interactive-pricing-component-main/index.js
@@ -1,16 +1,22 @@
 const btn = document.querySelector(".toggle-inner");
-const rate = document.getElementById("charge");
 const slide = document.querySelector(".slider-inner");
 const sliding = document.getElementById("slider-outer");
 const slidSurface = document.querySelector(".slider-surface");
-
-let mval = 16.00;
-let yval = mval*12 - (mval*12*25)/100;
-
-
+const views = document.getElementById("views");
+const charge = document.getElementById("charge");
+let x = 16.00;
+let y = 144.00;
 sliding.addEventListener("click",e=>{
     slidSurface.style.width=`${e.offsetX}px`;
     slide.style.left=`${e.offsetX>40? e.offsetX-25: e.offsetX-20}px`;
+    x=e.offsetX/4;
+    y=(x*12*75)/100;
+    views.innerText=`${e.offsetX}k`;
+    if(charge.nextElementSibling.innerText=="/year"){
+        charge.innerText=`$${y.toFixed(2)}`;
+    }else{
+        charge.innerText=`$${x.toFixed(2)}`;
+    }
 })
 
 window.addEventListener("resize",()=>{
@@ -22,8 +28,11 @@ btn.addEventListener("click",(event)=>{
     const el = event.target.classList;
     el.toggle("toggle-right");
     if(el.length==1){
-        rate.innerText=`$${Number(mval).toFixed(2)}`;
+        charge.innerText=`$${Number(x).toFixed(2)}`;
+        charge.nextElementSibling.innerText="/month";
     }else{
-        rate.innerText=`$${Number(yval).toFixed(2)}`;
+        charge.innerText=`$${Number((x*12*75)/100).toFixed(2)}`;
+        charge.nextElementSibling.innerText="/year";
     }
+    views.innerText=`${cnt}k`;
 })
