d16926adeae7213d0523b50e5d77fbc43af13d7e
Harsh Kumar <9u.harsh@gmail.com>
2022-09-12 12:46:58 +0530

    project preview

--- z-app/src/Components/Header.jsx
+++ z-app/src/Components/Header.jsx
@@ -1,25 +1,12 @@
 import React from "react";
-import Icon1 from "./icon-hamburger.svg";
-import Icon2 from "./icon-close.svg";
-import { useState } from "react";
 
 function Header() {
-  const [showdialogue, getdialogue] = useState(false);
-  const [setSrc, getSrc] = useState(Icon1);
-  function showBox() {
-    getdialogue(!showdialogue);
-    if (setSrc === Icon1) {
-      getSrc(Icon2);
-    } else {
-      getSrc(Icon1);
-    }
-  }
   return (
     <header>
       <div className="black">
         <nav>
           <h1>Project</h1>
-          <div className={showdialogue ? "nav-icon menu-full" : "nav-icon"}>
+          <div className="nav-icon">
             <iframe
               src={
                 "https://ghbtns.com/github-btn.html?user=thisisharsh7&type=follow&count=true&size=large"
@@ -41,12 +28,10 @@ function Header() {
               title={"GitHub"}
             ></iframe>
           </div>
-          <img
-            onClick={showBox}
-            className="hamburgerIcon"
-            src={setSrc}
-            alt="hamburger"
-          />
+          <div className="gitRepo">
+          
+            <a href="https://github.com/thisisharsh7">GitHub</a>
+          </div>
         </nav>
       </div>
     </header>
