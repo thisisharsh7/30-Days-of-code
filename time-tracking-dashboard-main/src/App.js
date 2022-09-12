11f041b18fcc74e1ac22327632ef333a8cbd4a17
Harsh Kumar <9u.harsh@gmail.com>
2022-09-13 00:37:57 +0530

    project 37 on go

--- time-tracking-dashboard-main/src/App.js
+++ time-tracking-dashboard-main/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./Components/Footer";
 import Main from "./Components/Main";
 
 const App = () => {
   return (
     <>
       <Main />
+      <Footer />
     </>
   );
 };
---
+++ time-tracking-dashboard-main/src/Components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div>
+          Challenge by <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://www.frontendmentor.io/profile/thisisharsh7">
+            Harsh Kumar
+          </a>
+          .
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
