9d7db0808d3f6973a8c5a859629fced25a4853be
Harsh Kumar <9u.harsh@gmail.com>
2022-09-12 12:10:56 +0530

    project preview

--- z-app/src/Components/Main.jsx
+++ z-app/src/Components/Main.jsx
@@ -1,288 +1,31 @@
 import React from "react";
 import Card from "./Card";
-import img1 from "./images/screenshot1.jpg";
-import img2 from "./images/screenshot2.jpg";
-import img3 from "./images/screenshot3.jpg";
-import img4 from "./images/screenshot4.jpg";
-import img5 from "./images/screenshot5.jpg";
-import img6 from "./images/screenshot6.jpg";
-import img7 from "./images/screenshot7.jpg";
-import img8 from "./images/screenshot8.jpg";
-import img9 from "./images/screenshot9.jpg";
-import img10 from "./images/screenshot10.jpg";
-import img11 from "./images/screenshot11.jpg";
-import img12 from "./images/screenshot12.jpg";
-import img13 from "./images/screenshot13.jpg";
-import img14 from "./images/screenshot14.jpg";
-import img15 from "./images/screenshot15.jpg";
-import img16 from "./images/screenshot16.jpg";
-import img17 from "./images/screenshot17.jpg";
-import img18 from "./images/screenshot18.jpg";
-import img19 from "./images/screenshot19.jpg";
-import img20 from "./images/screenshot20.jpg";
-import img21 from "./images/screenshot21.jpg";
-import img22 from "./images/screenshot22.jpg";
-import img23 from "./images/screenshot23.jpg";
-import img24 from "./images/screenshot24.jpg";
-import img25 from "./images/screenshot25.jpg";
-import img26 from "./images/screenshot26.jpg";
-import img27 from "./images/screenshot27.jpg";
-import img28 from "./images/screenshot28.jpg";
-import img29 from "./images/screenshot29.jpg";
-import img30 from "./images/screenshot30.jpg";
-import img31 from "./images/screenshot31.jpg";
-import img32 from "./images/screenshot32.jpg";
-import img33 from "./images/screenshot33.jpg";
-import img34 from "./images/screenshot34.jpg";
-import img35 from "./images/screenshot35.jpg";
-import img36 from "./images/screenshot36.jpg";
+import data from "../Components/data.json";
 
 function Main() {
   return (
     <>
       <div className="card-body">
-        <Card
-          display={img1}
-          goto="https://elegant-babka-9c3709.netlify.app/"
-          title="QR Code Card"
-          cnt="project1"
+      {
+        data && data.map((sdata) =>{
+          let pic = '';
+          try {
+            pic = require(`./images/screenshot${sdata.id}.jpg`);
+          }catch(err){
+            pic = require(`./images/noImage.webp`);
+          }
+          return(
+            <Card
+          display={pic}
+          goto={sdata.goto}
+          title={sdata.title}
+          cnt={sdata.cnt + sdata.id.toString()}
+          key={sdata.id}
         />
-
-        <Card
-          display={img2}
-          goto="https://sunny-crisp-7a9a2e.netlify.app/"
-          title="Product Preview Card"
-          cnt="project2"
-        />
-
-        <Card
-          display={img3}
-          goto="https://stellular-jalebi-3b1ecc.netlify.app/"
-          title="NFT Preview Card"
-          cnt="project3"
-        />
-
-        <Card
-          display={img4}
-          goto="https://chic-dieffenbachia-e4d655.netlify.app/"
-          title="Order Summary Card"
-          cnt="project4"
-        />
-
-        <Card
-          display={img5}
-          goto="https://courageous-lollipop-03872f.netlify.app/"
-          title="3 Column Preview Card"
-          cnt="project5"
-        />
-
-        <Card
-          display={img6}
-          goto="https://lucky-monstera-e9cc20.netlify.app/"
-          title="Profile Card Preview"
-          cnt="project6"
-        />
-
-        <Card
-          display={img7}
-          goto="https://creative-puppy-d58dc9.netlify.app/"
-          title="Huddle Landing Page"
-          cnt="project7"
-        />
-
-        <Card
-          display={img8}
-          goto="https://joyful-platypus-a2949d.netlify.app/"
-          title="Four Card Feature Preview"
-          cnt="project8"
-        />
-
-        <Card
-          display={img9}
-          goto="https://frabjous-sprinkles-a723d0.netlify.app/"
-          title="Single Price Grid Preview"
-          cnt="project9"
-        />
-
-        <Card
-          display={img10}
-          goto="https://musical-taffy-a1e27f.netlify.app/"
-          title="Social Proof Section Preview"
-          cnt="project10"
-        />
-
-        <Card
-          display={img11}
-          goto="https://chipper-kelpie-ffd450.netlify.app/"
-          title="Stats Preview Card"
-          cnt="project11"
-        />
-
-        <Card
-          display={img12}
-          goto="https://jolly-kitten-112311.netlify.app/"
-          title="Testimonial Grid Preview"
-          cnt="project12"
-        />
-
-        <Card
-          display={img13}
-          goto="https://astonishing-capybara-4b9445.netlify.app/"
-          title="Fylo Data Storage Preview"
-          cnt="project13"
-        />
-
-        <Card
-          display={img14}
-          goto="https://brilliant-platypus-ccc8fc.netlify.app/"
-          title="Huddle Full Landing Page"
-          cnt="project14"
-        />
-
-        <Card
-          display={img15}
-          goto="https://nimble-panda-389dda.netlify.app/"
-          title="Clipboard Landing Page"
-          cnt="project15"
-        />
-
-        <Card
-          display={img16}
-          goto="https://lucent-elf-3bf33f.netlify.app/"
-          title="Landing page fylo"
-          cnt="project16"
-        />
-
-        <Card
-          display={img17}
-          goto="https://super-yeot-edaf05.netlify.app/"
-          title="Huddle Landing page"
-          cnt="project17"
-        />
-
-        <Card
-          display={img18}
-          goto="https://lucky-salamander-e86340.netlify.app/"
-          title="Chat App Illustration"
-          cnt="project18"
-        />
-
-        <Card
-          display={img19}
-          goto="https://celebrated-clafoutis-5d42d2.netlify.app/"
-          title="FAQ Accordian Card"
-          cnt="project19"
-        />
-
-        <Card
-          display={img20}
-          goto="https://warm-panda-c8abe5.netlify.app/"
-          title="Form Validation"
-          cnt="project20"
-        />
-
-        <Card
-          display={img21}
-          goto="https://starlit-churros-5eb4ae.netlify.app/"
-          title="Ping single page"
-          cnt="project21"
-        />
-
-        <Card
-          display={img22}
-          goto="https://kaleidoscopic-genie-5b3b1b.netlify.app/"
-          title="Artice preview"
-          cnt="project22"
-        />
-
-        <Card
-          display={img23}
-          goto="https://fascinating-monstera-52135a.netlify.app/"
-          title="Apparel coming soon page"
-          cnt="project23"
-        />
-
-        <Card
-          display={img24}
-          goto="https://friendly-cat-327182.netlify.app/"
-          title="Interactive Rating Page"
-          cnt="project24"
-        />
-
-        <Card
-          display={img25}
-          goto="https://relaxed-gaufre-fc429b.netlify.app/"
-          title="Interactive card details"
-          cnt="project25"
-        />
-
-        <Card
-          display={img26}
-          goto="https://dashing-bublanina-3b9333.netlify.app/"
-          title="Sunnyside agency Landing Page"
-          cnt="project26"
-        />
-        <Card
-          display={img27}
-          goto="https://flourishing-concha-46ceef.netlify.app/"
-          title="Loopstudios Landing Page"
-          cnt="project27"
-        />
-
-        <Card
-          display={img28}
-          goto="https://heroic-druid-42014b.netlify.app/"
-          title="Insure Landing Page"
-          cnt="project28"
-        />
-        <Card
-          display={img29}
-          goto="https://neon-zuccutto-cc63a7.netlify.app/?"
-          title="Fylo Dark Theme Landing Page"
-          cnt="project29"
-        />
-        <Card
-          display={img30}
-          goto="https://subtle-zabaione-49b6c3.netlify.app/"
-          title="Blogr Landing Page"
-          cnt="project30"
-        />
-        <Card
-          display={img31}
-          goto="https://effortless-sunshine-6b3478.netlify.app/"
-          title="Bootcamp Testimonial slider"
-          cnt="project31"
-        />
-        <Card
-          display={img32}
-          goto="https://fluffy-paletas-994604.netlify.app/"
-          title="Pricing Component Toggle"
-          cnt="project32"
-        />
-        <Card
-          display={img33}
-          goto="https://incandescent-kleicha-b0b89f.netlify.app/"
-          title="Project Tracking Page"
-          cnt="project33"
-        />
-        <Card
-          display={img34}
-          goto="https://storied-mochi-bce03a.netlify.app/"
-          title="Intro Section Dropdown Menu"
-          cnt="project34"
-        />
-        <Card
-          display={img35}
-          goto="https://tourmaline-narwhal-a28b67.netlify.app/"
-          title="Interactive Rating Component"
-          cnt="project35"
-        />
-        <Card
-          display={img36}
-          goto="https://elaborate-eclair-c844ac.netlify.app/"
-          title="Tip Calculator"
-          cnt="project36"
-        />
+          )
+          
+        })
+      }
       </div>
     </>
   );
