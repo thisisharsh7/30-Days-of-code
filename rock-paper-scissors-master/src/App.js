01d6e9dccfb6b7bb9e43e092269c322b63a0cb3b
Harsh Kumar <9u.harsh@gmail.com>
2022-09-15 21:55:28 +0530

    project 39 on go

--- rock-paper-scissors-master/src/App.js
+++ rock-paper-scissors-master/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import Main from "./Components/Main.";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Main />
       <Footer />
     </>
   );
--- rock-paper-scissors-master/src/Components/Main..js
+++ rock-paper-scissors-master/src/Components/Main..js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import paper from "./images/icon-paper.svg";
+import scissors from "./images/icon-scissors.svg";
+import rock from "./images/icon-rock.svg";
 
-const Main = ()=> {
-    return(
-        <>
-            <main>
-                
-            </main>
-        </>
-    )
-}
+const Main = () => {
+  return (
+    <>
+      <main>
+      <div id="paper-outer">
+        <img src={paper} alt="paper" className="bg-icons"/>
+        </div>
+        <div id="rock-outer">
+        <img src={rock} alt="rock" className="bg-icons" />
+        </div>
+        <div id="scissors-outer">
+        <img src={scissors} alt="scissors" className="bg-icons"/>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Main;
