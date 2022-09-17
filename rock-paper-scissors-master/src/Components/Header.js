d95b5aa79d07d2cd3cf403c5858a34e012dda309
Harsh Kumar <9u.harsh@gmail.com>
2022-09-17 22:48:42 +0530

    project 39 on go

--- rock-paper-scissors-master/src/Components/Header.js
+++ rock-paper-scissors-master/src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <>
       <header>
         <div className="logo-score">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" id="logo"/>
           <p className="score-text">
             <span className="text">Score</span>
             <span id="score">12</span>
