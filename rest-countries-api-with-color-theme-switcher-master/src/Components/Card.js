58f1f171c9b9d609f124a9781ff86dc13c2b7c10
Harsh Kumar <9u.harsh@gmail.com>
2022-09-20 22:49:35 +0530

    project 40 on go

--- rest-countries-api-with-color-theme-switcher-master/src/Components/Card.js
+++ rest-countries-api-with-color-theme-switcher-master/src/Components/Card.js
@@ -4,12 +4,13 @@ export default function Card() {
   const [End, endPage] = useState(60);
   const [Start, startPage] = useState(0);
   const [Country, setCountry] = useState([]);
+  const [sample, sampleSet] = useState([]);
   const [data, sdata] = useState({});
   const getCountries = async () => {
     const response = await fetch("https://restcountries.com/v3.1/all");
-    setCountry(await response.json());
+    sampleSet(await response.json());
   };
-  console.log(Country);
+
   useEffect(() => {
     getCountries();
   }, []);
@@ -30,13 +31,85 @@ export default function Card() {
 
   const CountryDetail = (e) => {
     sdata(Country.find((scntry) => scntry.flags.svg === e.target.src));
-      document.querySelector(".outer-card").classList.toggle("beforeClick");
+    document.querySelector(".outer-card").classList.toggle("beforeClick");
     document.querySelector(".detCard").classList.toggle("newCard");
-    
   };
+  const getMore = (e) => {
+    sdata(
+      Country.find(
+        (scntry) =>
+          scntry?.cca2 === e.target.innerText ||
+          scntry?.cca3 === e.target.innerText ||
+          scntry?.cioc === e.target.innerText
+      )
+    );
+  };
+  useEffect(() => {
+    setCountry(sample);
+  }, [sample]);
+  useEffect(() => {
+    if (document.body.classList[0]) {
+      function fnc(sbtn) {
+        if (sbtn.classList[1] !== "lbtn") {
+          sbtn.classList.add("lbtn");
+        }
+      }
+      document.querySelectorAll(".skbtn").forEach(fnc);
+    }
+  });
+  function jadu(e) {
+    if (e.target.value !== "all") {
+      setCountry(
+        sample.filter((sel) => {
+          return sel.region === e.target.value;
+        })
+      );
+    } else {
+      setCountry(
+        sample.filter((sel) => {
+          return sel.region !== e.target.value;
+        })
+      );
+    }
+  }
+  function anotherJadu(e) {
+    const str = e.target.value;
+    const cstr = str.charAt(0).toUpperCase() + str.slice(1);
+    if (cstr !== "") {
+      setCountry(
+        sample.filter((sel) => {
+          return sel.name.common.match(cstr);
+        })
+      );
+    } else {
+      setCountry(
+        sample.filter((sel) => {
+          return sel.region !== cstr;
+        })
+      );
+    }
+  }
+
   return (
     <>
       <div className="outer-card">
+        <div className="short">
+          <input
+            className="nipt"
+            type="text"
+            onChange={anotherJadu}
+            placeholder="Search for a country..."
+          />
+          <label htmlFor="region">go: </label>
+          <select type="select" id="region" onChange={jadu}>
+            <option value="all">Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="Americas">Americas</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
         <div className="card-body">
           {Country.slice(Start, End).map((element, idx) => {
             return (
@@ -47,9 +120,7 @@ export default function Card() {
                   onClick={CountryDetail}
                 />
                 <div>
-                  <p>
-                    {element.name.common}
-                  </p>
+                  <p>{element.name.common}</p>
                   <p>
                     <span>Population: </span>
                     <span>{element.population}</span>
@@ -78,7 +149,8 @@ export default function Card() {
       </div>
       <div className="detCard">
         <button
-          type="button" className="tgbtn"
+          type="button"
+          className="tgbtn"
           onClick={() => {
             document.querySelector(".detCard").classList.toggle("newCard");
             document
@@ -89,13 +161,17 @@ export default function Card() {
           Go Back
         </button>
         <div className="scountry-detail card">
-          <img src={data?.flags?.svg} alt="flag" />
+          <img src={data?.flags?.png} alt="flag" />
           <div>
             <p>{data?.name?.common}</p>
             <p>
-                    <span>Native Name: </span>
-                    <span>{(data.name?.nativeName?.eng?.official) ? (data.name?.nativeName?.eng?.official) : (data?.name?.official)}</span>
-                  </p>
+              <span>Native Name: </span>
+              <span>
+                {data.name?.nativeName?.eng?.official
+                  ? data.name?.nativeName?.eng?.official
+                  : data?.name?.official}
+              </span>
+            </p>
             <p>
               <span>Population: </span>
               <span>{data?.population}</span>
@@ -118,15 +194,40 @@ export default function Card() {
             </p>
             <p>
               <span>Currencies: </span>
-              <span>{(data.currencies) ? (Object.values(data.currencies)[0]?.name) : "No Data"}</span>
+              <span>
+                {data.currencies
+                  ? Object.values(data.currencies)[0]?.name
+                  : "No Data"}
+              </span>
             </p>
             <p>
               <span>Languages: </span>
-              <span>{(data.languages) ? (Object.values(data.languages).map(slang=>{ return `${slang}, `})) : ""}</span>
+              <span>
+                {data.languages
+                  ? Object.values(data.languages).map((slang) => {
+                      return `${slang}, `;
+                    })
+                  : ""}
+              </span>
             </p>
             <p>
               <span>Border Countries: </span>
-              <span>{(data.borders) ? (data.borders.map(sborder => {return `${sborder} ,`})) : ""}</span>
+              <span className="removBug">
+                {data.borders
+                  ? data.borders.map((sborder, idx) => {
+                      return (
+                        <button
+                          className="  skbtn"
+                          type="button"
+                          key={idx}
+                          onClick={getMore}
+                        >
+                          {sborder}
+                        </button>
+                      );
+                    })
+                  : "None"}
+              </span>
             </p>
           </div>
         </div>
--- rest-countries-api-with-color-theme-switcher-master/src/Components/Header.js
+++ rest-countries-api-with-color-theme-switcher-master/src/Components/Header.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Header() {
+  const [mode , setMode] = useState("Light");
   const DarkWhite = () =>{
     document.querySelector("header").classList.toggle("whiteBgK");
     document.body.classList.toggle("whiteBg");
     document.querySelectorAll(".card").forEach(scard=> scard.classList.toggle("whiteBgK"));
-    document.querySelectorAll("button").forEach(sbtn=>sbtn.classList.toggle("lbtn"));
+    document.querySelectorAll(".tgbtn").forEach(sbtn=>sbtn.classList.toggle("lbtn"));
+    document.querySelectorAll(".skbtn").forEach(sbtn=>sbtn.classList.toggle("lbtn"));
+    document.querySelector(".nipt").classList.toggle("whiteBgK");
+    document.querySelector("#region").classList.toggle("whiteBgK");
+     (mode === "Dark") ? setMode("Light") : setMode("Dark");
   }
+
   return (
     <>
       <header>
         <div>
           <p>Where in the world?</p>
-          <button type="button" onClick={DarkWhite}>Switch to Dark Mode</button>
+          <button type="button" onClick={DarkWhite}>Switch to {mode} Mode</button>
         </div>
       </header>
     </>
