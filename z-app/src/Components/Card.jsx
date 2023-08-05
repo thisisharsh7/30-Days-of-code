900f47589211a28d0949ccad69ce09175b85f287
Harsh Kumar <9u.harsh@gmail.com>
2023-08-06 02:10:50 +0530

    animation and images update

--- z-app/src/Components/Card.jsx
+++ z-app/src/Components/Card.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 function Card(props) {
+  const ref = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"]
+  })
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
   return (
-    <>
+    <motion.div ref={ref} style={{
+      scale: scaleProgress,
+      opacity: opacityProgress
+    }}>
       <div className="card">
         <a href={props.goto} target="_blank" rel="noreferrer" >
-          <img className="img-1" src={props.display} alt={props.cnt} />
+          <LazyLoadImage src={props.display}
+            width={250} height={180}
+            placeholderSrc={props.placeHolder}
+            effect="blur"
+            className="img-1"
+            alt={props.cnt}
+          />
           <p>{props.title}</p>
         </a>
       </div>
-    </>
+    </motion.div>
   );
 }
 
--- z-app/src/Components/Main.jsx
+++ z-app/src/Components/Main.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import Card from "./Card";
 import data from "../Components/data.json";
+import { useScroll, motion } from "framer-motion";
 
 function Main() {
+  const { scrollYProgress } = useScroll()
   return (
     <>
+      <motion.div style={{
+        scaleX: scrollYProgress, position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 30,
+        height: "6px",
+        backgroundColor: "white",
+        borderBlock: "2px solid black",
+        transformOrigin: 0
+      }} />
       <div className="card-body" >
         {
-          
-          data && data.reverse().map((sdata) =>{
-          let pic = '';
-        try {
-          pic = require(`./images/screenshot${sdata.id}.jpg`);
-          }catch(err){
-          pic = require(`./images/noImage.webp`);
-          }
-        return(
-        <Card
-          display={pic}
-          goto={sdata.goto}
-          title={sdata.title}
-          cnt={sdata.cnt + sdata.id.toString()}
-          key={sdata.id}
-        />
-        )
-          
-        })
-      }
+
+          data && data.reverse().map((sdata, index) => {
+            let pic = '';
+            let placeHolder = '';
+            try {
+              pic = require(`./images/screenshot${sdata.id}.jpg`);
+              placeHolder = require(`./images/placeScreenshot${sdata.id}.jpg`)
+            } catch (err) {
+              pic = require(`./images/noImage.webp`);
+              placeHolder = require(`./images/noImage.webp`)
+            }
+            return (
+              <Card
+                display={pic}
+                placeHolder={placeHolder}
+                goto={sdata.goto}
+                title={sdata.title}
+                cnt={sdata.cnt + sdata.id.toString()}
+                key={sdata.id}
+              />
+            )
+
+          })
+        }
       </div>
     </>
   );
